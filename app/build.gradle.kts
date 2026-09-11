0fd0953a5922e38978eb7a61e5e56d393295b20d
tantanredmi10 <tantanredmi10@gmail.com>
2026-09-12 06:43:49 +0700

    Prepare signed release build v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,15 @@ android {
         applicationId = "com.tansglobal.autoresponder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.1.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
